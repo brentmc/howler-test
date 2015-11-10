e23f6a00e7b3e6cfba0beb2b75ef8556f8e6a658
brentmc <brentmcivor.mail+workgithub@gmail.com>
2015-11-10 15:16:56 +1000

    Now has chained audio to simulate constructing a sentence from a bunch of individual word mp3s

--- example/HowerTest.js
+++ example/HowerTest.js
@@ -1,48 +1,83 @@
 /**
  * Created by brentmcivor on 10/11/15.
  */
+import Howler from 'howler'
+
 class HowlerTest{
+    constructor(){
+        this.soundsAr = []
+    }
 
+    setup() {
+        this.setupBurp()
+        this.setupMunch()
+        this.setupLoopingBG()
+        this.setupCountdown()
+    }
 
-    setup(){
-
-        this._isLooping = false
-
+    setupBurp(){
         let burpBtn = document.getElementById('burpBtn')
-        let munchBtn = document.getElementById('munchBtn')
-        let loopBtn = document.getElementById('bgSoundBtn')
-
-
         let burp = new Howl({
-            urls:['example/assets/burp.mp3'],
-            onend:() => console.log('onBurpEnd')})
+            urls: ['example/assets/burp.mp3'],
+            onend: () => console.log('onBurpEnd')
+        })
         burpBtn.onclick = () => burp.play()
+    }
 
-        let munch = new Howl({urls:['example/assets/munch.mp3']})
+    setupMunch(){
+        let munchBtn = document.getElementById('munchBtn')
+        let munch = new Howl({urls: ['example/assets/munch.mp3']})
         munchBtn.onclick = () => munch.play()
+    }
 
-        let loopingBG = new Howl({urls:['example/assets/ocean_bg.mp3'], loop:true})
+    setupLoopingBG(){
+        this._isLooping = false
+        let loopBtn = document.getElementById('bgSoundBtn')
+        let loopingBG = new Howl({urls: ['example/assets/ocean_bg.mp3'], loop: true})
         loopBtn.onclick = () => {
-            if(this._isLooping){
+            if (this._isLooping) {
                 console.log('time to pause')
                 this._isLooping = false
                 loopingBG.pause()
-            }else{
+            } else {
                 console.log('time to play')
                 this._isLooping = true
                 loopingBG.play()
             }
-
         }
-
-
-
-
-
-
-
-
     }
+
+    playNextNumber(){
+        console.log('playNextNumber() - this.soundsAr = ', this.soundsAr)
+        // this.soundsAr.forEach(sound => console.log('02 sound = ', sound))
+
+        if(this.soundsAr && this.soundsAr.length > 0){
+            let tempSound = this.soundsAr.shift()
+            tempSound.onend = () => this.playNextNumber()
+            tempSound.play()
+        }else{
+            console.log('soundAr is null or empty')
+        }
+    }
+
+    setupCountdown(){
+        let countBtn = document.getElementById('countBtn')
+
+        this.soundsAr = []
+        let numbersAr = ['0', '1', '2', '3', 4, 5, 6, 7, 8, 9, 10]
+        numbersAr.forEach(num => {
+            console.log('num = ' + num)
+            this.soundsAr.push(new Howl(
+                {   urls: ['example/assets/' + num + ".mp3"],
+                    onend:() => this.playNextNumber()
+                }))
+        });
+
+        this.soundsAr.forEach(sound => console.log('01 sound = ', sound))
+
+        countBtn.onclick = () => this.playNextNumber()
+    }
+
 }
 
 export default HowlerTest
--- example/app.js
+++ example/app.js
@@ -1,12 +1,9 @@
-import Howler from 'howler'
 import HowlerTest from './HowerTest'
 
 const init = config => {
   var howlerTest = new HowlerTest()
   howlerTest.setup()
 }
-
-
 
 module.exports = {
   init:init
