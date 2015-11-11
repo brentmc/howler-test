7fde40fa6adf8733f6e5fc6890727d530dacc69d
brentmc <brentmcivor.mail+workgithub@gmail.com>
2015-11-11 11:15:45 +1000

    No longer using DOM elements and adding canvas elements instead

--- example/app.js
+++ example/app.js
@@ -1,8 +1,20 @@
 import HowlerTest from './HowerTest'
 
 const init = config => {
-  var howlerTest = new HowlerTest()
-  howlerTest.setup()
+
+  var canvas = document.getElementById('tutorial');
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+  }
+
+ // var howlerTest = new HowlerTest()
+ // howlerTest.setup()
 }
 
 module.exports = {
